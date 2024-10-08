cfeab2eeea9737aa14d733cedb0da1c6e1242d6e
edernavarro <edernavarro@gmail.com>
2024-10-08 14:22:32 -0500

    configuring vnc server

--- fedora-kde-vncserver-config.sh
+++ fedora-kde-vncserver-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Actualizar el sistema
 sudo dnf update -y
 
@@ -8,8 +7,8 @@ sudo dnf update -y
 sudo dnf install -y tigervnc-server
 
 # Verificar si startplasma-x11 está disponible
-if ! command -v startplasma &> /dev/null; then
-    echo "startplasma no está instalado. Instalando plasma-desktop..."
+if ! command -v startplasma-x11 &> /dev/null; then
+    echo "startplasma-x11 no está instalado. Instalando plasma-desktop..."
     sudo dnf install -y plasma-desktop
 fi
 
@@ -45,14 +44,36 @@ XSTARTUP_FILE="$HOME/.vnc/xstartup"
 cat <<EOL > $XSTARTUP_FILE
 #!/bin/sh
 
-# Iniciar el entorno de escritorio KDE
+# Iniciar el entorno de escritorio KDE en X11
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-exec startplasma
+
+# Iniciar D-Bus si no está corriendo
+if [ -z "\$DBUS_SESSION_BUS_ADDRESS" ]; then
+  eval \$(dbus-launch --sh-syntax --exit-with-session)
+fi
+
+exec startplasma-x11 &
 EOL
 
 # Hacer que el archivo xstartup sea ejecutable
 chmod +x $XSTARTUP_FILE
+
+# Desactivar Wayland en GDM (Gestor de Pantallas)
+if [ -f /etc/gdm/custom.conf ]; then
+    echo "Desactivando Wayland en GDM"
+    sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm/custom.conf
+else
+    echo "Creando archivo de configuración de GDM y desactivando Wayland"
+    cat <<EOL | sudo tee /etc/gdm/custom.conf
+[daemon]
+WaylandEnable=false
+EOL
+fi
+
+# Reiniciar GDM para aplicar los cambios
+echo "Reiniciando GDM para aplicar los cambios..."
+sudo systemctl restart gdm
 
 # Permitir el puerto VNC en el firewall
 sudo firewall-cmd --permanent --add-port=5901/tcp
@@ -66,4 +87,3 @@ echo "Logs del servidor VNC:"
 cat ~/.vnc/*.log
 
 echo "Configuración completa. Puedes conectarte a la máquina virtual usando un cliente VNC en la dirección: <tu_direccion_ip>:5901"
-echo "También puedes conectarte mediante XRDP en el puerto 3389."
